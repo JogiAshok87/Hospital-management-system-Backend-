ca2beccfc3a702a640430ab3f3861874d6b6c3b5
Ashok <113456041+JogiAshok87@users.noreply.github.com>
2025-10-01 13:24:42 +0530

    first commit

--- server.js
+++ server.js
@@ -14,8 +14,10 @@ import winston from 'winston';
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin:"*"
-  
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  optionsSuccessStatus: 200
 }));
 
 
